b5129c4138d1c341a5fdea46355b4f5852226267
evgeny <bykovzhukov@yandex.ru>
2018-09-06 18:05:21 +0300

    new data

--- src/js/data.js
+++ src/js/data.js
@@ -5,20 +5,19 @@ export default {
     title: 'Картинка не загрузилась',
     description: 'Угадайте, что изображено на пиксельном изображении',
     questions: [{
-        title: '',
         msg: 'Это LADA.',
         images: [{
-            img: 'img/q/2/1.png',
-            img2x: 'img/q/2/1.png',
+            img: 'https://leonardo.osnova.io/cde0c5ed-87a2-e39e-14fd-1b43c90a6d50/',
+            img2x: 'https://leonardo.osnova.io/6bbca432-10a8-f1d0-35d2-cd5e79cfd2a4/',
         }, {
-            img: 'img/q/2/2.png',
-            img2x: 'img/q/2/2.png',
+            img: 'https://leonardo.osnova.io/2783a9e6-c699-0a82-2156-0f9d0f904d69/',
+            img2x: 'https://leonardo.osnova.io/7d0c97a1-30d7-e7dd-a89f-231f3fe7636e/',
         }, {
-            img: 'img/q/2/3.png',
-            img2x: 'img/q/2/3.png',
+            img: 'https://leonardo.osnova.io/909994ed-1aef-f226-911c-4ddb2712dba1/',
+            img2x: 'https://leonardo.osnova.io/439f529b-a063-7309-0d75-e9e81222b44a/',
         }, {
-            img: 'img/q/2/4.png',
-            img2x: 'img/q/2/4.png',
+            img: 'https://leonardo.osnova.io/8f3e04c2-6a50-bfe3-4e6b-287de1646a0b/',
+            img2x: 'https://leonardo.osnova.io/760a36d7-c435-75a4-6f73-b33fd677f607/',
         }],
         options: [{
             text: 'LADA',
@@ -31,72 +30,44 @@ export default {
             text: 'Jaguar'
         }]
     }, {
-        title: '',
-        msg: 'Это рубли.',
+        msg: 'Это Греф-инвалид.',
         images: [{
-            img: 'img/q/3/1.png',
-            img2x: 'img/q/3/1.png',
+            img: 'https://leonardo.osnova.io/6de63272-1221-8edb-8ce8-c18fa84c54f5/',
+            img2x: 'https://leonardo.osnova.io/a0709e94-4bfa-c97a-3e69-304c3b9170f1/',
         }, {
-            img: 'img/q/3/2.png',
-            img2x: 'img/q/3/2.png',
+            img: 'https://leonardo.osnova.io/023299f4-5406-1abb-a193-b0ee7311bd9d/',
+            img2x: 'https://leonardo.osnova.io/f6fee303-04e4-98d6-0625-ff66519c503e/',
         }, {
-            img: 'img/q/3/3.png',
-            img2x: 'img/q/3/3.png',
+            img: 'https://leonardo.osnova.io/35a4f326-762d-32ef-d14c-aa4a9ac9f60e/',
+            img2x: 'https://leonardo.osnova.io/7a67b68b-72d5-d90f-448c-2550bfbd0121/',
         }, {
-            img: 'img/q/3/4.png',
-            img2x: 'img/q/3/4.png',
+            img: 'https://leonardo.osnova.io/38d22e80-7c05-54dc-0e6f-f6f50c1a0ecd/',
+            img2x: 'https://leonardo.osnova.io/5de8b6fe-74c8-0c6f-77f3-9c5a7383ae1c/',
         }],
         options: [{
-            text: 'Доллары'
-        }, {
-            text: 'Евро'
-        }, {
-            text: 'Йены'
-        }, {
-            text: 'Рубли',
-            isCorrect: true
-        }]
-    }, {
-        title: '',
-        msg: 'Это Павел Дуров с голым торсом.',
-        images: [{
-            img: 'img/q/10/1.jpg',
-            img2x: 'img/q/10/1@2x.jpg',
-        }, {
-            img: 'img/q/10/2.jpg',
-            img2x: 'img/q/10/2@2x.jpg',
-        }, {
-            img: 'img/q/10/3.jpg',
-            img2x: 'img/q/10/3@2x.jpg',
-        }, {
-            img: 'img/q/10/4.jpg',
-            img2x: 'img/q/10/4@2x.jpg',
-        }],
-        options: [{
-            text: 'Лысый из Brazzers'
-        }, {
-            text: 'Лысая собака'
-        }, {
-            text: 'Дуров с голым торсом',
+            text: 'Греф-инвалид',
             isCorrect: true
         }, {
-            text: 'Коленка'
+            text: 'Робокоп'
+        }, {
+            text: 'Шварцнейггер'
+        }, {
+            text: 'Том Харди'
         }]
     }, {
-        title: '',
         msg: 'Это лого «МегаФона».',
         images: [{
-            img: 'img/q/1/1.png',
-            img2x: 'img/q/1/1.png',
+            img: 'https://leonardo.osnova.io/01858f90-6af7-af0b-05c2-f40c67f4de29/',
+            img2x: 'https://leonardo.osnova.io/aa3c5165-30b9-0270-5e55-2dd1d11ce489/',
         }, {
-            img: 'img/q/1/2.png',
-            img2x: 'img/q/1/2.png',
+            img: 'https://leonardo.osnova.io/73909884-9473-f361-2a15-b19d911f75e5/',
+            img2x: 'https://leonardo.osnova.io/712c5d91-5d3c-4a74-0f62-988c665e03dc/',
         }, {
-            img: 'img/q/1/3.png',
-            img2x: 'img/q/1/3.png',
+            img: 'https://leonardo.osnova.io/a3f92840-162b-b517-af08-e5a7bd478694/',
+            img2x: 'https://leonardo.osnova.io/2c77c58c-b1c5-0d9f-2b8f-b6267e5960ae/',
         }, {
-            img: 'img/q/1/4.png',
-            img2x: 'img/q/1/4.png',
+            img: 'https://leonardo.osnova.io/491bea03-2dc5-d725-02a7-5562653ae206/',
+            img2x: 'https://leonardo.osnova.io/aef23e87-8f36-7f92-8acf-048e87a94c3d/',
         }],
         options: [{
             text: 'Лого «МегаФона»',
@@ -107,6 +78,181 @@ export default {
             text: 'Лого WhatsApp'
         }, {
             text: 'Лого Delivery Club'
+        }]
+    }, {
+        msg: 'Это рубли.',
+        images: [{
+            img: 'https://leonardo.osnova.io/a3efcbca-349d-ca62-69fa-23c28548b2b0/',
+            img2x: 'https://leonardo.osnova.io/ee9c3b44-25c2-d8ae-c06b-164b11fbaedc/',
+        }, {
+            img: 'https://leonardo.osnova.io/9e1c8de5-66af-bee8-9c60-3dd80d01d0da/',
+            img2x: 'https://leonardo.osnova.io/a31d254b-90cb-b937-e6cd-a71401eb49e7/',
+        }, {
+            img: 'https://leonardo.osnova.io/7a0d2df9-f8fc-33c2-d173-1f619cf7eb88/',
+            img2x: 'https://leonardo.osnova.io/4e9a495d-34db-e2b8-fda6-d8d9d40a6b68/',
+        }, {
+            img: 'https://leonardo.osnova.io/650a384b-3465-42bf-a7b1-c2a3e0b0657d/',
+            img2x: 'https://leonardo.osnova.io/d029a5c2-b420-3533-0aa9-be7a9046243d/',
+        }],
+        options: [{
+            text: 'Рубли',
+            isCorrect: true
+        }, {
+            text: 'Доллары'
+        }, {
+            text: 'Евро'
+        }, {
+            text: 'Йены'
+        }]
+    }, {
+        msg: 'Это синее платье.',
+        images: [{
+            img: 'https://leonardo.osnova.io/cecd86ef-6c36-53e2-34e3-a153337cb2d7/',
+            img2x: 'https://leonardo.osnova.io/b980d4cf-ed38-bfa7-87ff-1c8d6d8974b0/',
+        }, {
+            img: 'https://leonardo.osnova.io/34a23e1e-206e-a07d-90d2-78dae6756b35/',
+            img2x: 'https://leonardo.osnova.io/44c39a02-5769-a7d7-4326-e509eb13c523/',
+        }, {
+            img: 'https://leonardo.osnova.io/afb1517a-7d2b-82d2-ad54-423bce1fdbae/',
+            img2x: 'https://leonardo.osnova.io/9acffbf9-52bf-a0ee-8a00-5240971da594/',
+        }, {
+            img: 'https://leonardo.osnova.io/e37177ba-7e80-f1a0-658a-470c9d8d8af1/',
+            img2x: 'https://leonardo.osnova.io/13313e92-08a7-9a34-bdd1-d918ad0eb441/',
+        }],
+        options: [{
+            text: 'Синее платье',
+            isCorrect: true
+        }, {
+            text: 'Белое платье'
+        }, {
+            text: 'Янни'
+        }, {
+            text: 'Лорел'
+        }]
+    }, {
+        msg: 'Это дискета.',
+        images: [{
+            img: 'https://leonardo.osnova.io/41d69d6e-c3e3-699a-695c-494524ed0d9d/',
+            img2x: 'https://leonardo.osnova.io/9efd735f-ee29-1647-92c6-3aeb83f81408/',
+        }, {
+            img: 'https://leonardo.osnova.io/2596597a-a891-717a-56dd-8dd2cfe262eb/',
+            img2x: 'https://leonardo.osnova.io/0f85b496-d12f-e170-0d0b-d2abb6b3cba2/',
+        }, {
+            img: 'https://leonardo.osnova.io/2596597a-a891-717a-56dd-8dd2cfe262eb/',
+            img2x: 'https://leonardo.osnova.io/0f85b496-d12f-e170-0d0b-d2abb6b3cba2/',
+        }, {
+            img: 'https://leonardo.osnova.io/2053980b-3c9f-368e-81a7-44d2370a1174/',
+            img2x: 'https://leonardo.osnova.io/93c533c9-443d-c696-0939-ff8dfeedadaa/',
+        }],
+        options: [{
+            text: 'Дискета',
+            isCorrect: true
+        }, {
+            text: '«Черный квадрат»'
+        }, {
+            text: 'Карманный ПК'
+        }, {
+            text: 'Печенье'
+        }]
+    }, {
+        msg: 'Это роскомнадзор.',
+        images: [{
+            img: 'https://leonardo.osnova.io/4c449935-8ed5-bf93-8cbc-be47dc56959f/',
+            img2x: 'https://leonardo.osnova.io/16e24103-f7d4-bc58-d147-5c6b3e41bc8d/',
+        }, {
+            img: 'https://leonardo.osnova.io/481682d2-df01-7105-db1e-c281eca3403b/',
+            img2x: 'https://leonardo.osnova.io/389f757f-95de-b1ee-c16f-bd4bfca3ae81/',
+        }, {
+            img: 'https://leonardo.osnova.io/ebd1bda0-a963-724b-9eab-7f0a476293dc/',
+            img2x: 'https://leonardo.osnova.io/c0725b5d-ba00-a66f-113e-b2ab9aad608d/',
+        }, {
+            img: 'https://leonardo.osnova.io/ba1ded58-dfaf-4a40-114a-833477259dc5/',
+            img2x: 'https://leonardo.osnova.io/d8b6f1e2-6cf5-c074-514f-abb218ef7169/',
+        }],
+        options: [{
+            text: 'Роскомнадзор',
+            isCorrect: true
+        }, {
+            text: 'ВКонтакте'
+        }, {
+            text: 'Facebook'
+        }, {
+            text: 'Telegram'
+        }]
+    }, {
+        msg: 'Это milliondollarhomepage.com.',
+        images: [{
+            img: 'https://leonardo.osnova.io/6888febc-f046-682a-5644-5359918832ae/',
+            img2x: 'https://leonardo.osnova.io/c9274fca-ea3b-4977-190f-c61cf4387d8a/',
+        }, {
+            img: 'https://leonardo.osnova.io/01367270-7055-4182-375b-db61597541d7/',
+            img2x: 'https://leonardo.osnova.io/bf6609f6-e9a1-2e4e-89ab-6d24583e87c5/',
+        }, {
+            img: 'https://leonardo.osnova.io/59ab2da6-1941-1e1d-b8fb-6ed42d03c4f1/',
+            img2x: 'https://leonardo.osnova.io/6ca3a604-0340-a373-793a-d8357f354ae9/',
+        }, {
+            img: 'https://leonardo.osnova.io/c27e6ab0-a4f0-28a9-3a7b-30bd2e2de756/',
+            img2x: 'https://leonardo.osnova.io/eb35b1e6-da8d-e150-11db-b21a016e5ffe/',
+        }],
+        options: [{
+            text: 'milliondollarhomepage.com',
+            isCorrect: true
+        }, {
+            text: 'Афиша Дня города Волоколамска'
+        }, {
+            text: 'Полотно пользователей Reddit'
+        }, {
+            text: 'Рассыпанный Скитлз'
+        }]
+    }, {
+        msg: 'Это Дуров с голым торсом.',
+        images: [{
+            img: 'https://leonardo.osnova.io/6743beec-d5fd-ddbe-683b-57274c72dbea/',
+            img2x: 'https://leonardo.osnova.io/fb70ee8c-fa8f-24ba-8124-e40af6406050/',
+        }, {
+            img: 'https://leonardo.osnova.io/d2895eea-232b-df0d-d67f-c5968ea5391b/',
+            img2x: 'https://leonardo.osnova.io/bf60a6a5-4dd1-475f-4f37-6643f0958172/',
+        }, {
+            img: 'https://leonardo.osnova.io/839abd90-7c2f-3795-e3d4-b3712bf32ad4/',
+            img2x: 'https://leonardo.osnova.io/820fce64-29a6-d303-633c-9f304444b466/',
+        }, {
+            img: 'https://leonardo.osnova.io/f233d8f9-1c9c-292b-4dde-109ecad080a2/',
+            img2x: 'https://leonardo.osnova.io/df1a2ccb-881c-fc82-9bb2-5df53e654d75/',
+        }],
+        options: [{
+            text: 'Дуров с голым торсом',
+            isCorrect: true
+        }, {
+            text: 'Лысый из Brazzers'
+        }, {
+            text: 'Лысая собака'
+        }, {
+            text: 'Коленка'
+        }]
+    }, {
+        msg: 'Это Falcon 9.',
+        images: [{
+            img: 'https://leonardo.osnova.io/158ab1fc-05e6-d2a8-d55f-6a3d960cfa3b/',
+            img2x: 'https://leonardo.osnova.io/ffbdb075-08c2-2136-41be-143de11fee3f/',
+        }, {
+            img: 'https://leonardo.osnova.io/96b5571a-5c4c-0cfc-beb0-eb5641786080/',
+            img2x: 'https://leonardo.osnova.io/bcd504bf-34e4-869f-ddfc-0380f8ec659e/',
+        }, {
+            img: 'https://leonardo.osnova.io/142d955a-dea2-fddf-2b87-3a5c483a8334/',
+            img2x: 'https://leonardo.osnova.io/d7139c72-2c0c-55af-3ac2-b31f83643393/',
+        }, {
+            img: 'https://leonardo.osnova.io/65a0f63c-da7f-8801-9517-62d36d718520/',
+            img2x: 'https://leonardo.osnova.io/d2c2d970-55ab-893e-c467-6d5beafc43c3/',
+        }],
+        options: [{
+            text: 'Falcon 9',
+            isCorrect: true
+        }, {
+            text: 'Средний палец'
+        }, {
+            text: 'Ким Кардашьян на пляже'
+        }, {
+            text: 'Ядерный гриб'
         }]
     }],
     results: [{
